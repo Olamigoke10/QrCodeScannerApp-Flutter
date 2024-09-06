1f3b0d194518ea9c90be274db2ce7eedadade1a2
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-06 23:27:08 +0100

    Setting up the project

---
+++ lib/Qr.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    // TODO: implement createState
+    return _HomePageState();
+  }
+}
+
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed:  setState(() {
+              Navigator.of(context).push
+            }),
+           child: const Text('Scan Qr Code'),
+           ),
+           const SizedBox(height: 40,)
+           ElevatedButton(
+            onPressed: setState(() {
+             
+           }),
+            child: Text('Generate Qr Code')),
+        ],
+      ),
+    )
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "QR Code Scanner and Generator",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("QR code Scanner and generator"),
+          backgroundColor: Colors.blue,
+        ),
+        body: ,
+      ),
+    )
+  );
+}
